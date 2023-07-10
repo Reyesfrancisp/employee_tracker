fe5acc9666ed784a12a2847d301cde2d16955502
Francis Reyes <reyesfrancisp@gmail.com>
2023-07-10 01:53:52 -0400

    Added some starter code for the display in the employee list

--- server.js
+++ server.js
@@ -1,7 +1,87 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
-const questions = [{
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'your_username',
+  password: 'your_password',
+  database: 'employee_tracker_db'
+});
 
-    
-}]
+// Connect to the MySQL server
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ', err);
+    return;
+  }
+  console.log('Connected to the database!');
+  // Prompt user for the desired action
+  promptUser();
+});
+
+// Function to prompt the user for the action to perform
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit'
+        ]
+      }
+    ])
+    .then((answers) => {
+      // Perform the selected action
+      switch (answers.action) {
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+          break;
+        case 'Exit':
+          connection.end(); // Close the database connection
+          console.log('Disconnected from the database.');
+          break;
+      }
+    });
+}
+
+// Function to view all departments
+function viewAllDepartments() {
+  connection.query('SELECT * FROM department', (err, results) => {
+    if (err) {
+      console.error('Error retrieving departments: ', err);
+      return;
+    }
+    console.log('All Departments:');
+    console.table(results);
+    promptUser();
+  });
+}
